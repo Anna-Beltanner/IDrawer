ba15d134b24162cb25d02368806b70d663b54323
Anna Naumova <51020604+Anna-Beltanner@users.noreply.github.com>
2020-12-19 22:27:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/idrawer/IDrawerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.idrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IDrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_i_drawer)
+    }
+}
